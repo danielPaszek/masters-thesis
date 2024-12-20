318a0b2ac736796cb62ef275a3b41af3694e7f75
danielPaszek <daniel.paszek@student.put.poznan.pl>
2024-12-20 19:37:26 +0100

    20-12 - started preprocessing

--- preprocesssing/easy_raw_cleanup.py
+++ preprocesssing/easy_raw_cleanup.py
@@ -5,19 +5,22 @@ import re
 from util import *
 
 tickers = os.listdir('../data/raw')
+dumpPath = '../data/processed/easy_raw_cleanup'
 
 for ticker in tickers:
     csvFiles = os.listdir('../data/raw/'+ticker)
-    for csvFile in csvFiles:
+    for i, csvFile in enumerate(csvFiles):
         print(ticker + csvFile)
-        df = pd.read_csv('../data/raw/'+ticker+'/'+csvFile, header=None, keep_default_na=False)
-        df = cleanFirstNumericRows(df)
+        df = pd.read_csv(os.path.join('../data/raw', ticker, csvFile), header=None, keep_default_na=False)
         # drop x-1.csv files
-        if len(df.index) < 5:
+        if len(df.index) < 8 or (re.search('^.*-[1-9].csv$', csvFile) and len(df.index) < 15):
             continue
 
+        df = cleanFirstNumericRows(df)
         df, period = cleanHeaderAndGetPeriod(df)
-        print(df.iloc[0,:])
-    break
+        df, unit = parseAnotherHeader(df)
+        dirPath = os.path.join(dumpPath, ticker, str(period), str(unit))
+        os.makedirs(dirPath, exist_ok=True)
+        df.to_csv(os.path.join(dirPath, str(i) + '.csv'), header=False, index=False)
 
-
+    print(ticker + ' done')
--- preprocesssing/util.py
+++ preprocesssing/util.py
@@ -1,25 +1,50 @@
 import re
+import pandas as pd
+
+THOUSAND = 'thousand'
+MILLION = 'million'
+USD = 'usd'
+
 
 def cleanFirstNumericRows(df):
-    while True:
-        wrong = [val == '' or val.isnumeric() for val in df.iloc[0, :]]
-        if all(wrong):
-            df = df.iloc[1:].reset_index(drop=True)
-        else:
-            break
+    while all([x.isnumeric() or x == '' for x in df.iloc[0, 1:]]):
+        df = df.iloc[1:].reset_index(drop=True)
+    # while True:
+    #     wrong = [val == '' or val.isnumeric() for val in df.iloc[0, :]]
+    #     if all(wrong):
+    #         df = df.iloc[1:].reset_index(drop=True)
+    #     else:
+    #         break
     return df
+
+
+def trashRow(df):
+    return all([x == ''
+                or 'see note' in x.lower()
+                or 'may not sum ' in x.lower()
+                or 'intersegment revenues are not material' in x.lower()
+                or 'based on continuing operations' in x.lower()
+                or 'may not add ' in x.lower()
+                or 'does not foot' in x.lower()
+                or 'net of income taxes.' in x.lower()
+                or re.search('^\[[0-9]\]$', x)
+                or re.search('^\([0-9]\)$', x)
+                or re.search('^\{[0-9]\}$', x)
+                for x in df.iloc[0, 1:]])
+
 
 # https://www.geeksforgeeks.org/python-find-most-frequent-element-in-a-list/
 def most_frequent(List):
     return max(set(List), key=List.count)
 
 
-# group by months period and need to get unit (millions etc). .? for capitalizing
-def cleanHeaderAndGetPeriod(df):
+def cleanHeaderAndGetPeriod(df: pd.DataFrame):
     period = -1
+    while trashRow(df):
+        df = df.iloc[1:].reset_index(drop=True)
     try:
         while True:
-            periodTags = [re.search('^[0-9]+.?.?onths.?.?nd.*$', val) for val in df.iloc[0, :]]
+            periodTags = [re.search('^[0-9]+.?months.?end.*$', val.lower()) for val in df.iloc[0, :]]
             periodTags = [x for x in periodTags if x]
             if not any(periodTags):
                 raise Exception('No period found - inside')
@@ -29,5 +54,34 @@ def cleanHeaderAndGetPeriod(df):
             df = df.iloc[1:].reset_index(drop=True)
     except Exception as e:
         if period == -1:
+            # junk row on top
+            if any([len(x) > 40 for x in df.iloc[0, :]]) or all([x.isnumeric() for x in df.iloc[0, 1:]]):
+                df = df.iloc[1:].reset_index(drop=True)
+                return cleanHeaderAndGetPeriod(df)
             raise Exception('No period found')
     return df, period
+
+
+def parseAnotherHeader(df: pd.DataFrame):
+    isThousand = [re.search('^.*thousand.*$', val.lower()) for val in df.iloc[0, :]]
+    thousandIndexes = [i for i, val in enumerate(isThousand) if bool(val)]
+    thousandCount = len(thousandIndexes)
+    isMillion = [re.search('^.*million.*$', val.lower()) for val in df.iloc[0, :]]
+    millionIndexes = [i for i, val in enumerate(isMillion) if bool(val)]
+    millionCount = len(millionIndexes)
+    isUsd = [re.search('^.*usd.*$', val.lower()) for val in df.iloc[0, :]]
+    usdIndexes = [i for i, val in enumerate(isUsd) if bool(val)]
+
+    if thousandCount == 0 and millionCount == 0 and len(usdIndexes) == 0:
+        print('ANOTHER HEADER FAILED TO GET UNIT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+        raise Exception('ANOTHER HEADER')
+    if thousandCount == 0 and millionCount == 0:
+        res = USD
+    elif thousandCount > millionCount:
+        res = THOUSAND
+    else:
+        res = MILLION
+    indexes = thousandIndexes + millionIndexes + usdIndexes
+    for i in indexes:
+        df.iloc[0, i] = ''
+    return df, res
